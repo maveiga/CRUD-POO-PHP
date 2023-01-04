07d4736a719573087f2c417e024b37472c6717e5
mateus a <mateusaugustot17@gmail.com>
2023-01-03 23:41:06 -0300

    initial commit

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>wdev vagas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  </head>
+  <body class="bg-dark text-light">
+    <div class="container">
+        <div class="jumbotrom bg-danger">
+            <h1>WDEV vagas</h1>
+            <p>Crud em php orientado a objetos</p>
+        </div>
+
+    
+    
+   
+ 
